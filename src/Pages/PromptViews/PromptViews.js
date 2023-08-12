b7dd057acbb3f1578745760466698c46820784d4
Gerald Villalta Rojas <villaltageral@gmail.com>
2023-08-12 15:49:22 -0600

    Búsqueda de prompts

--- src/Pages/PromptViews/PromptViews.js
+++ src/Pages/PromptViews/PromptViews.js
@@ -1,9 +1,12 @@
 import PromptTable from "../../Components/PromptTable/PromptTable";
 import { PromptsGet } from "../../Datos/Prompts/PromptsGet";
+import { PromptsSearch } from "../../Datos/Prompts/PromptsSearch";
 import { PromptsDelete } from "../../Datos/Prompts/PromptsDelete";
 import { AlertMessage } from "../../Components/AlertMessage/AlertMessage";
-
+import Spinner from "../../Components/Spinner/Spinner";
+import './PromptViews.scss'
 import React, { useEffect, useState } from 'react';
+import Select from 'react-select';
 import { useNavigate } from 'react-router-dom';
 
 export function PromptViews() {
@@ -11,12 +14,52 @@ export function PromptViews() {
 
     const navigate = useNavigate();
     const [showAlert, setShowAlert] = useState(false);
+    const [message, setMessage] = useState("");
+    const [typeAlert, setTypeAlert] = useState("");
+    const [showNameInput, setShowNameInput] = useState(false);
+    const [showTypeInput, setShowTypeInput] = useState(false);
+    const [showTagsInput, setShowTagsInput] = useState(false);
+
+    const [nameValue, setNameValue] = useState('');
+    const [typeValue, setTypeValue] = useState('');
+
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [selectedOpcionTags, setSelectedOpcionTags] = useState([]);
+    const tags = ["Programación", "Inteligencia Artificial", "Ciberseguridad", "Big Data", "Redes", "Cardiología", "Dermatología", "Neurología", "Pediatria", "Cirugía", "Matemáticas", "Historia", "Ciencias", "Idiomas", "Arte"];
+
+    const handleNameCheckboxChange = () => {
+        setShowNameInput(!showNameInput);
+    };
+
+    const handleTypeCheckboxChange = () => {
+        setShowTypeInput(!showTypeInput);
+    };
+
+    const handleTagsCheckboxChange = () => {
+        setShowTagsInput(!showTagsInput);
+    };
+
+    const handleNameInputChange = (event) => {
+        setNameValue(event.target.value);
+    };
+
+    const handleTypeInputChange = (event) => {
+        setTypeValue(event.target.value);
+    };
+
+    const handleTagChange = selectedOptions => {
+        const selectedValues = selectedOptions.map(option => option.value);
+        setSelectedOpcionTags(selectedValues);
+        setSelectedTags(selectedOptions);
+    };
+
+    const options = tags.map(tag => ({ value: tag, label: tag }));
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const userResponse = await PromptsGet();
-                setPrompts(userResponse);
+                const promptResponse = await PromptsGet();
+                setPrompts(promptResponse);
             } catch (error) {
                 console.log(error);
             }
@@ -24,9 +67,26 @@ export function PromptViews() {
         fetchData();
     }, []);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if ((showNameInput || showTagsInput || showTypeInput)) {
+            console.log(selectedOpcionTags)
+            if ((nameValue || (typeValue && typeValue !== "") || selectedOpcionTags.length > 0)) {
+                const promptResponse = await PromptsSearch(showNameInput ? nameValue : null, showTypeInput ? typeValue : null, showTagsInput ? selectedOpcionTags : null);
+                console.log(promptResponse.data.searchPrompt);
+                setPrompts(promptResponse.data.searchPrompt);
+            }
+        } else {
+            setShowAlert(true);
+            setMessage("You must enter some data to be able to search");
+            setTypeAlert("danger");
+        }
+    }
+
     const userDelete = () => {
         setShowAlert(true);
-
+        setMessage("Prompts eliminated successfully");
+        setTypeAlert("success");
     }
 
     const onView = (promptId) => {
@@ -43,29 +103,55 @@ export function PromptViews() {
     }
     return (
         <div className="container_table">
-            {showAlert && (<AlertMessage showAlert={showAlert} setShowAlert={setShowAlert} message={"Prompts eliminated successfully"} variant={"success"} />)}
+            {showAlert && (<AlertMessage showAlert={showAlert} setShowAlert={setShowAlert} message={message} variant={typeAlert} />)}
             <div className="container text-center">
-                <div className="row">
-                    <div className="col col-lg-2">
-                        <button onClick={() => navigate(`/promptnew`)}><i className="fa-regular fa-square-plus fa-2xl" style={{ color: '#ffffff', }} /></button>
-                    </div>
-                    <div className="col-md-auto">
-                        <span>Filter:  </span>
-                        <input type="checkbox" className="btn-check" id="btn-check-Name" autoComplete="off" />
-                        <label className="btn btn-outline-primary" htmlFor="btn-check-Name">Name</label>
-                        <input type="checkbox" className="btn-check" id="btn-check-Type" autoComplete="off" />
-                        <label className="btn btn-outline-primary" htmlFor="btn-check-Type">Type</label>
-                        <input type="checkbox" className="btn-check" id="btn-check-Tags" autoComplete="off" />
-                        <label className="btn btn-outline-primary" htmlFor="btn-check-Tags">Tags</label>
-                    </div>
-                    <div className="col">
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <div className="row row-cols-4">
+                    <form className="d-flex" role="search" onSubmit={handleSubmit}>
+                        <div className="col-md-auto">
+                            <div className="input-group mb-3">
+                                <div className="input-group-text">
+                                    <input type="checkbox" className="btn-check" id="btn-check-Name" autoComplete="off" onChange={handleNameCheckboxChange} />
+                                    <label className="btn btn-outline-primary" htmlFor="btn-check-Name">Name</label>
+                                </div>
+                                {showNameInput && (
+                                    <input className="form-control" type="text" id="nameInput" value={nameValue} onChange={handleNameInputChange} />
+                                )}
+                            </div>
+                        </div>
+                        <div className="col-md-auto">
+                            <div className="input-group mb-3">
+                                <div className="input-group-text">
+                                    <input type="checkbox" className="btn-check" id="btn-check-Type" autoComplete="off" onChange={handleTypeCheckboxChange} />
+                                    <label className="btn btn-outline-primary" htmlFor="btn-check-Type">Type</label>
+                                </div>
+                                {showTypeInput && (
+                                    <select className="form-select" id="type" name="type" value={typeValue} onChange={handleTypeInputChange} >
+                                        <option value="">Select type</option>
+                                        <option value="Edit">Edit</option>
+                                        <option value="Images">Image</option>
+                                        <option value="Completitions">Completitions</option>
+                                    </select>
+                                )}
+                            </div>
+                        </div>
+                        <div className="col-md-auto">
+                            <div className="input-group mb-3">
+                                <div className="input-group-text">
+                                    <input type="checkbox" className="btn-check" id="btn-check-Tags" autoComplete="off" onChange={handleTagsCheckboxChange} />
+                                    <label className="btn btn-outline-primary" htmlFor="btn-check-Tags">Tags</label>
+                                </div>
+                                {showTagsInput && (
+                                    <Select className="form-control" isMulti options={options} value={selectedTags} onChange={handleTagChange} placeholder="Select tags" />
+                                )}
+                            </div>
+                        </div>
+                        <div className="col col-lg-2">
                             <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
+            <button onClick={() => navigate(`/promptnew`)}><i className="fa-regular fa-square-plus fa-2xl" style={{ color: '#ffffff', }} />  NEW</button>
 
             <PromptTable prompts={prompts} onView={onView} onEdit={onEdit} onDelete={onDelete} />
         </div>
